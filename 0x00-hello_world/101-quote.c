9e6e7f5587f2f68be114a0b14485cf6886abed0a
David Orozco <dvdizcky@gmail.com>
2020-02-05 16:56:49 -0500

    101 fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr, "%s", s);
 return (1);
 }
