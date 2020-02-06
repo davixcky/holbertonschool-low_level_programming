cc5172054b33078df58e446b0a2b487e29a2cf05
David Orozco <dvdizcky@gmail.com>
2020-02-06 17:39:02 -0500

    4 added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+			continue;
+
+		putchar(i);
+		if (i == 'z')
+			putchar('\n');
+	}
+	return (0);
+}
+
