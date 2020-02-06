5c60578ec8d9551abfb1ab075b626393ec3819ff
David Orozco <dvdizcky@gmail.com>
2020-02-06 16:37:44 -0500

    3 added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 'a';
+	j = 'z';
+
+	for (i; i <= j; i++)
+	{
+		putchar(i);
+
+		if (i == 122)
+		{
+			i = 64;
+			j = 'Z';
+		}
+
+		if (i == 'Z')
+			putchar('\n');
+	}
+	return (0);
+}
