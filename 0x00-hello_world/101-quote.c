10e17f4c67d25d18cb8846372f6081ab9dc9e643
David Orozco <dvdizcky@gmail.com>
2020-02-05 22:54:03 -0500

    Bit fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 int len = sizeof(s) / sizeof(s[0]);
-write(2, s, len);
+write(2, s, len-1);
 return (1);
 }
