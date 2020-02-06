9b1a07cc44000eb6da3ed91ebd27085af8ea278e
David Orozco <dvdizcky@gmail.com>
2020-02-06 18:15:55 -0500

    9 added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+
+		putchar(i);
+
+		if (i == '9')
+		{
+			putchar('\n');
+			break;
+		}
+
+		putchar(',');
+		putchar(' ');
+
+	}
+	return (0);
+}
+
