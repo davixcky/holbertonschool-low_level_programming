61db23bd4f94f2f5aaa2df4ce92a9032b80cdf38
David Orozco <dvdizcky@gmail.com>
2020-02-06 15:01:31 -0500

    Exercise 2 added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+	* main - Entry point
+	*
+	* Return: Always 0 (Success)
+	*/
+
+int main(void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+		if (i == 'z')
+			putchar('\n');
+	}
+
+	return (0);
+}
