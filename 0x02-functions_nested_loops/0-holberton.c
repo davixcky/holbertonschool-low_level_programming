b7b6982b35fcfd406e478775245364543f5af1dd
David Orozco <dvdizcky@gmail.com>
2020-02-11 13:54:31 +0000

    New line fixed

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,17 +9,15 @@
 int main(void)
 {
 	char *text;
-	int len, i;
+	int i;
 
-	text = "Holberton";
-	len = strlen(text);
+	text = "Holberton\n";
 
-	for (i = 0; i <= len; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(text[i]);
 	}
 
-	_putchar('\n');
 
 	return (0);
 }
