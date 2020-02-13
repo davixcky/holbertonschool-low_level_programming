a179f66c66c4176c7ef2875aabb40fa05b2360bc
David Orozco <dvdizcky@gmail.com>
2020-02-13 12:49:45 -0500

    10 created

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * print_spaces - Print spaces
+ * @n: Number of times the spaces
+ */
+void print_spaces(int n)
+{
+	for (; n > 0; n--)
+		_putchar(' ');
+}
+
+
+/**
+ * print_character - Print character #
+ * @n: Number of times the #
+ */
+void print_character(int n)
+{
+	for (; n > 0; n--)
+		_putchar('#');
+}
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, sp;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+
+
+	sp = size;
+	for (i = 1; i <= size; i++)
+	{
+		print_spaces(sp);
+		print_character(i);
+
+		sp--;
+
+		_putchar('\n');
+	}
+}
+
