40f2fd87ed478c6dfbc06b34cb3a40fcdae6ff66
David Orozco <dvdizcky@gmail.com>
2020-02-12 19:37:17 -0500

    Fixed formula

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
-
-/**
- * calculate_fibonacci - Calculate the fibonacci series
- */
-void calculate_fibonacci(void)
-{
-	int counter;
-
-	long double i, res, aux;
-
-	i = 1;
-	res = 2;
-
-	printf("1, 2");
-	for (counter = 1; counter <= 96; counter++)
-	{
-		aux = res + i;
-		i = res;
-		res = aux;
-
-		printf(", %.0Lf", aux);
-	}
-	printf("\n");
-}
-
+#define N 1000000000
 
 /**
  * main - Entry point
- * Return: Return always 0
+ * Return: Always 0
  */
 int main(void)
 {
-	calculate_fibonacci();
+	unsigned long a, b, r, i, a1, a2, b1, b2, r1, r2;
+
+	a = 1;
+	b = 2;
+	r = a + b;
+
+	printf("%lu, %lu, ", a, b);
+	for (i = 0; i < 86; i++)
+	{
+		printf("%lu, ", r);
+		a = b;
+		b = r;
+		r = a + b;
+	}
+
+	b1 = b / N;
+	b2 = b % N;
+	r1 = r / N;
+	r2 = r % N;
+
+	for (i = 0; i < 9; i++)
+	{
+		printf("%lu%lu, ", r1, r2);
+		a1 = b1;
+		a2 = b2;
+		b1 = r1;
+		b2 = r2;
+
+		r1 = a1 + b1 + (a2 + b2) / N;
+		r2 = (a2 + b2) % N;
+	}
+
+	printf("%lu%lu\n", r1, r2);
+
+
 	return (0);
 }
 
