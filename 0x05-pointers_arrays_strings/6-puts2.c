5c9942b012816e4b6497f29ce0601bf17417cce5
David Orozco <dvdizcky@gmail.com>
2020-02-18 09:16:51 -0500

    Condition changed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,15 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
 
+		i++;
+	}
 	_putchar('\n');
 
 }
+
