01361c32da3351c20cd0983a5ede7aff09308ee3
David Orozco <dvdizcky@gmail.com>
2020-02-18 11:58:26 -0500

    Condition deleted

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,11 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	if (n <= 0)
-		return;
-
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
 		{
-			printf("%d\n", a[i]);
+			printf("%d", a[i]);
 		}
 		else
 		{
@@ -25,4 +21,7 @@ void print_array(int *a, int n)
 		}
 	}
 
+	printf("\n");
+
 }
+
