cbb003d890f77ae2e12b40980562a869282cfc00
David Orozco <dvdizcky@gmail.com>
2020-02-20 16:39:53 -0500

    7 created

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @s: String
+ * Return: String
+ */
+char *leet(char *s)
+{
+	char *lower_alpha, *upper_alpha, *numbers;
+	int i, j;
+
+	lower_alpha = "aeotl";
+	upper_alpha = "AEOTL";
+	numbers = "43071";
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j <= 5; j++)
+			if (s[i] == lower_alpha[j] || s[i] == upper_alpha[j])
+				s[i] = numbers[j];
+
+	return (s);
+}
+
