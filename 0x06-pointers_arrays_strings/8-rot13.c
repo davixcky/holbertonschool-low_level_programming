343f31d42c00c8687d545368da16f035582f3266
David Orozco <dvdizcky@gmail.com>
2020-02-20 20:49:46 -0500

    8 created and fixed

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @s: String to enconde
+ * Return: String encode
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char *normal, *rot13;
+
+	normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; normal[j] != '\0'; j++)
+		{
+			if (s[i] == normal[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
