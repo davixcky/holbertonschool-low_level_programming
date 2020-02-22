ff94080eeec40c4dc64d9cfc4a8df2aef1079e6e
David Orozco <dvdizcky@gmail.com>
2020-02-21 20:12:45 -0500

    103 added

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * print_buffer - Print buffer
  * @b: Char
@@ -7,4 +7,25 @@
  */
 void print_buffer(char *b, int size)
 {
+	int i, j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j += 2)
+		{
+			if (i + j < size)
+				printf("%02x%02x ", b[i + j], b[i + j + 1]);
+			else
+				printf("     ");
+		}
+		for (j = 0; j < 10 && i + j < size; j++)
+		{
+			if (b[i + j] < 32)
+				printf(".");
+			else
+				printf("%c", b[i + j]);
+		}
+		printf("\n");
+	}
 }
