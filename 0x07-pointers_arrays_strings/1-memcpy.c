9371c84a1d004bbb397f4d00244bce0221b3bd8a
David Orozco <dvdizcky@gmail.com>
2020-02-24 12:28:33 -0500

    Iterator added

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,9 +10,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
-	for (; n > 0 && src && dest; n--, src++, dest++)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		*dest = *src;
+		dest[i] = src[i];
 	}
 
 	return (dest);
