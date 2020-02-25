4349da5883c22055603e2bdf0cbfce0d488adad9
David Orozco <dvdizcky@gmail.com>
2020-02-24 19:12:07 -0500

    5 typo

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,13 +4,13 @@ char *_strstr(char *haystack, char *needle)
 {
 	int j;
 
-	for (; *haystack != '\0'; s++)
+	for (; *haystack != '\0'; haystack++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (*haystack == needle[j])
 			{
-				return (s);
+				return (haystack);
 			}
 		}
 
