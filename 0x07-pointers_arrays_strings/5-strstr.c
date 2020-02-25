7889bc8fe95edd620628061ffacfe7e71c6b01a5
David Orozco <dvdizcky@gmail.com>
2020-02-24 21:03:39 -0500

    5 added

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,20 +1,51 @@
 #include "holberton.h"
+#include <stdio.h>
+
+/**
+ *  * _strlen - Len of a string
+ *   * @s: String
+ *    * Return: Length
+ *     */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
+
 
 char *_strstr(char *haystack, char *needle)
 {
-	int j;
-
-	for (; *haystack != '\0'; haystack++)
+	int letter, needle_size;
+	char *pos;
+	letter = 0;
+	needle_size = _strlen(needle);
+	for (; *haystack != 0; haystack++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		if (*haystack == needle[0])
 		{
-			if (*haystack == needle[j])
+			pos = haystack;
+			for (; *needle != 0 && *haystack != 0; needle++, haystack++)
 			{
-				return (haystack);
+				if (*haystack == *needle)
+				{
+					letter++;
+				}
+				else
+				{
+					letter = 0;
+				}
 			}
 		}
-
 	}
 
-	return (0);
+	if (letter == needle_size)
+		return (pos);
+
+	return (haystack);
 }
