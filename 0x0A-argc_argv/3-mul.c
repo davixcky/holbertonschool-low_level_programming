74c0e9baf34d31e9b06b912fb0be1ec6618dfa89
David Orozco <dvdizcky@gmail.com>
2020-02-29 13:18:31 -0500

    Condition changed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,17 +12,16 @@ int main(int argc, char **argv)
 {
 	int n1, n2;
 
-	if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	if (argc >= 3)
+	if (argc == 3)
 	{
 		n1 = atoi(argv[1]);
 		n2 = atoi(argv[2]);
 		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 
 	return (0);
