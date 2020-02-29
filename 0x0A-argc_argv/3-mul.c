a2147d8c6185b62f866a59417bb0f3253c7847cd
David Orozco <dvdizcky@gmail.com>
2020-02-29 13:15:26 -0500

    3 betty style and fixed arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,20 @@
 int main(int argc, char **argv)
 {
 	int n1, n2;
-	if (argc < 2)
 
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
+	if (argc >= 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
 
-	printf("%d\n", n1 * n2);
 	return (0);
 }
 
