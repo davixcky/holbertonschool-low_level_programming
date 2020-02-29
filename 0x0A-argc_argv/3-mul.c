fa7e2b4cfb10e383c77ddba1faa16e81555eb16c
David Orozco <dvdizcky@gmail.com>
2020-02-29 12:22:48 -0500

    Variables in top

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,13 @@
  */
 int main(int argc, char **argv)
 {
+	int n1, n2;
 	if (argc < 2)
+
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int n1, n2;
 
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[2]);
