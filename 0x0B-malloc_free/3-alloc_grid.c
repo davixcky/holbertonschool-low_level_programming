b21f5f6cd48458e7794c8ff034957bf98f0256cf
David Orozco <dvdizcky@gmail.com>
2020-03-03 14:28:16 -0500

    fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *)malloc(width * sizeof(int));
+		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 			return (NULL);
 	}
