a45952ad3525db5419c9e381c0a5a265d88b7802
David Orozco <dvdizcky@gmail.com>
2020-03-03 16:20:35 -0500

    Malloc deleted

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,13 +30,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = malloc(1);
-		*s1 = ' ';
+		s1 = " ";
 	}
 	if (s2 == NULL)
 	{
-		s2 = malloc(1);
-		*s2 = ' ';
+		s2 = " ";
 	}
 
 	size_s1 = _strlen(s1);
