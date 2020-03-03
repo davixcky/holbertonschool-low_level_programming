0de8e2080e1b4c39fef79a146f1dda17410d6fd4
David Orozco <dvdizcky@gmail.com>
2020-03-03 12:31:04 -0500

    Strlen deleted

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,22 @@
 #include "holberton.h"
 
+/**
+ *  * _strlen - Len of a string
+ *   * @s: String
+ *    * Return: Length
+ *     */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
+
 char *_strdup(char *str)
 {
 	int size, i;
