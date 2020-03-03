d569b373c96b48a3217fa557cdb096e7baf9f197
David Orozco <dvdizcky@gmail.com>
2020-03-03 14:26:40 -0500

    position of array equal to null verify

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,9 +15,10 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)malloc(width * sizeof(int));
-		if (arr == NULL)
+		if (arr[i] == NULL)
 			return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
