ee3147b0ee632e4186b27e4cdba7de9e7dc18b9a
David Orozco <dvdizcky@gmail.com>
2020-03-03 13:04:00 -0500

    Variable i declared

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,17 +3,17 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **arr;
+	int **arr, i, j;
 
 	if (!width || !height)
 		return (NULL);
 
 	arr = malloc(height * sizeof(int *));
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 		arr[i] = (int *)malloc(width * sizeof(int));
 
-	for (int i = 0; i < height; i++)
-		for (int j = 0; j < width; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 
 	return (arr);
