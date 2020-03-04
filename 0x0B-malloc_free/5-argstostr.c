634604f114dd06d3a311f104c6e466e1ddcb7e8e
David Orozco <dvdizcky@gmail.com>
2020-03-03 19:00:23 -0500

    5 added

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Len of a string
+ * @s: String
+ * Return: Length
+ **/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
+/**
+ * argstostr - Concatenates all the arguments of your program
+ * @ac: Length of av
+ * @av: Array of strings
+ * @dest: Array of strings
+ **/
+char *argstostr(int ac, char **av)
+{
+	char *words;
+	int len, i, r, c;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0, len = 0; i < ac; i++)
+		len += _strlen(av[i]) + 1;
+
+	words = malloc(len);
+	if (words == NULL)
+		return (NULL);
+
+	i = 0;
+	for (r = 0; r < ac; r++)
+	{
+		for (c = 0; av[r][c] != 0; c++)
+		{
+			words[i] = av[r][c];
+			i++;
+		}
+
+		words[i] = '\n';
+		i++;
+	}
+
+	return (words);
+}
