77db5ef9bb84e10537df0b5d6e5555614dd856b8
David Orozco <dvdizcky@gmail.com>
2020-03-03 22:31:02 -0500

    Betty style applied

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
 
+/**
+ * free_grid - Frees a 2 dimensional grid previously created
+ * @grid: 2 dimensional arrays
+ * @height: Size
+ **/
 void free_grid(int **grid, int height)
 {
 	int i;
