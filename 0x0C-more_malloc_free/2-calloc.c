4a06a44ba3ea5d9bbf87d2b1078d9ffc88f22a42
David Orozco <dvdizcky@gmail.com>
2020-03-05 13:41:17 -0500

    2 added

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] =  0;
+	}
+
+	return (ptr);
+}
