ef89432db68e86b0be3776e160bab0d3cb601c39
David Orozco <dvdizcky@gmail.com>
2020-03-05 14:04:55 -0500

    2 changed to char

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,7 +2,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *ptr;
+	char *ptr;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < nmemb * size; i++)
 	{
 		ptr[i] =  0;
 	}
