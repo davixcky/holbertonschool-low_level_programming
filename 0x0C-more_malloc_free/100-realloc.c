2e93973ccbe2a60edf7e183e48219bfd616b9241
David Orozco <dvdizcky@gmail.com>
2020-03-05 15:28:59 -0500

    1st advanced created

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - Copy a string to another
+ * @dest: Destination copy
+ * @src: Source for copy
+ * Return: Return the value of dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s;
+
+	s = malloc(old_size);
+
+	if (new_size > old_size)
+	{
+		_strcpy(s, ptr);
+		free(ptr);
+
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+
+		_strcpy(ptr, s);
+		free(s);
+		return (ptr);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+
+		return (ptr);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	return (NULL);
+}
