a732cd31d70a787a980dc87e80a08df56ae57d0b
David Orozco <dvdizcky@gmail.com>
2020-03-09 13:39:28 -0500

    check the pointer

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,11 @@
 #include "dog.h"
-#include <string.h>
-#include <stdlib.h>
-
+#include <stddef.h>
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = malloc(sizeof(name));
-	d->owner = malloc(sizeof(owner));
-
-	strcpy(d->name, name);
-	strcpy(d->owner, owner);
-
-	d->age = age;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->owner = owner;
+		d->age = age;
+	}
 }
