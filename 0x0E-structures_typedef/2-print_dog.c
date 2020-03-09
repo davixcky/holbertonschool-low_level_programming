80b64fec144f24cb4e788da042bb14d49b474f74
David Orozco <dvdizcky@gmail.com>
2020-03-09 13:56:07 -0500

    check on age deleted

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,17 +2,16 @@
 #include <stddef.h>
 #include <stdio.h>
 
+/**
+ * print_dog -
+ * @d:
+ **/
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
 
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-
-	if (d->age)
-		printf("Age: %f\n", d->age);
-	else
-		printf("Age: (nil)\n");
-
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
