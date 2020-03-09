4eb300f60245ae87514f5accd0e9834233b92f1a
David Orozco <dvdizcky@gmail.com>
2020-03-09 13:41:46 -0500

    betty style aplied 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,13 @@
 #include "dog.h"
 #include <stddef.h>
+
+/**
+ * init_dog - Initialize a variable of type struct dog
+ * @d: Pointer to struct dog
+ * @name: Animal name
+ * @age: Animal age
+ * @owner: Animal owner
+ **/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
