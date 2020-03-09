8d3b9ff3054de8313661fa026caac31c7f351306
David Orozco <dvdizcky@gmail.com>
2020-03-09 13:31:54 -0500

    malloc check

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,9 +8,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t* p;
 
 	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
 
 	p->name = malloc(sizeof(name));
+	if (p->name == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+
 	p->owner = malloc(sizeof(owner));
+	if (p->owner == NULL)
+	{
+		free(p->name);
+		free(p);
+		return (NULL);
+	}
 
 	strcpy(p->name, name);
 	strcpy(p->owner, owner);
