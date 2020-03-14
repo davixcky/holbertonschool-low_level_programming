39e4fd2e71e577029dfcfbd2a4fff1872e1147fd
David Orozco <dvdizcky@gmail.com>
2020-03-14 10:53:57 -0500

    Betty style added and 3 fixed

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,24 @@
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int, ...);
+void print_all(const char * const, ...);
+
+void print_string(va_list);
+void print_char(va_list);
+void print_integer(va_list);
+void print_float(va_list);
+
+
+/**
+ * struct opt - Typedef struct
+ *
+ * @op: Format
+ * @f: The function associated
+ **/
+typedef struct opt
+{
+	char *op;
+	void (*f)(va_list);
+} p_opt;
 
 #endif
