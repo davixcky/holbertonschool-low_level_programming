91fe548ed76c48e865f8bf8f343e13cfdf75f721
David Orozco <dvdizcky@gmail.com>
2020-03-14 12:40:27 -0500

    n 0 handled

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,19 +10,19 @@ void print_strings(const char *s, const unsigned int n, ...)
 	int i;
 	va_list str;
 	char *aux;
+	const char *aux2;
 
 	va_start(str, n);
 
-	for (i = 0; i < (int) n - 1; i++)
+	aux2 = (s != NULL) ? s : "";
+	for (i = 0; i < (int) n; i++)
 	{
 		aux = va_arg(str, char*);
 		printf("%s%s", (aux != NULL) ? aux : "(nil)",
-				(s == NULL) ? "" : s);
+				(i < (int) n - 1) ? aux2 : "");
 	}
 
-	aux = va_arg(str, char*);
-	printf("%s\n", (aux != NULL) ? aux : "(nil)");
-
+	putchar(10);
 
 	va_end(str);
 }
