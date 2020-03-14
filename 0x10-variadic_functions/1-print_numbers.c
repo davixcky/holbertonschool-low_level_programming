cf8e299f027d1bae57d7eae01f20941e5b59629b
David Orozco <dvdizcky@gmail.com>
2020-03-14 12:30:41 -0500

    n equal to zero

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(nums, n);
 
-	if ((int) n < 0)
+	if ((int) n <= 0)
+	{
+		printf("\n");
 		return;
+	}
 
 	for (i = 0; i < (int) n - 1; i++)
 		printf("%d%s", va_arg(nums, int), (separator == NULL) ? "" : separator);
