cf2a0d77dc68661eab97c49a47ad8ad7d9d7f16a
David Orozco <dvdizcky@gmail.com>
2020-03-19 13:15:58 -0500

    Betty style added

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
  **/
 void __attribute__ ((constructor)) _100first(void)
 {
-	printf("You're beat! and yet, you must allow,\n" \
+	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
 }
