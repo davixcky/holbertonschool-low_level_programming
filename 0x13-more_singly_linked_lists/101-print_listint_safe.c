8f11a940c0458ed286587fe22a080eac2dddba6b
David Orozco <dvdizcky@gmail.com>
2020-03-24 17:22:20 -0500

    Betty style

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: Head of the list
+ *
+ * Return: Numbers of elements of the list
+ **/
 size_t print_listint_safe(const listint_t *head)
 {
 	int i, flag;
