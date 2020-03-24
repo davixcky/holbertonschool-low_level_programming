d8aa9f8f203a6cfc948d5b12443ac50472f7d82c
David Orozco <dvdizcky@gmail.com>
2020-03-24 17:45:09 -0500

    3 avanced free added

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+size_t free_listint_safe(listint_t **head)
+{
+	int i, flag;
+	listint_t *tmp;
+
+	for (i = flag = 0; *head != NULL || flag; i++)
+	{
+		if (*head <= (*head)->next)
+		{
+			flag = 1;
+			break;
+		}
+
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		flag = 0;
+	}
+
+	if (flag)
+	{
+		tmp = (*head)->next;
+		free(*head);
+	}
+
+	*head = NULL;
+	return (i);
+}
