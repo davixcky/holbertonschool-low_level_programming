34f49124ece8537d9008d7658159295518c2859b
David Orozco <dvdizcky@gmail.com>
2020-03-24 09:40:53 -0500

    Betty style added

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * delete_nodeint_at_index - Deletes the node at index index of a
+ * listint_t linked list
+ * @head: Reference to the start of the list
+ * @index: Index of the node that should be deleted. Index starts at 0
+ *
+ * Return: 1 if was deleted and -1 if dont was deleted
+ **/
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	int i;
@@ -20,3 +28,4 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	return (-1);
 }
+
