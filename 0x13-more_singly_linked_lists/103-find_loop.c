a251450f650e01567435f1801ecb2c77e6ad3a07
David Orozco <dvdizcky@gmail.com>
2020-03-24 17:15:21 -0500

    advanced uploaded

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *fast, *slow;
+
+	if (head == NULL)
+		return (NULL);
+
+	slow = head;
+	fast = head->next;
+
+	while (slow != fast)
+	{
+		if (fast.next == NULL || slow == NULL)
+			return (NULL);
+
+		slow = slow.next;
+		fast= head->next;
+	}
+
+	return (slow);
+}
