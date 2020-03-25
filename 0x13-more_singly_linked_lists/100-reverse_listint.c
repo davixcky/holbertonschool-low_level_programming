83ce573d8454cc6983a3b465d812dd921b8d555b
David Orozco <dvdizcky@gmail.com>
2020-03-24 20:54:08 -0500

    First advanced added

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,5 +2,18 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *prev, *current, *next;
 
+	prev = next = NULL;
+	current = *head;
+
+	while (current != NULL) {
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+
+	return (*head);
 }
