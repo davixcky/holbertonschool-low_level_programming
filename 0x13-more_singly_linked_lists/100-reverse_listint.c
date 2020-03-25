23116332bd5f5c12d1960046ffd83d82c256cb47
David Orozco <dvdizcky@gmail.com>
2020-03-24 21:03:12 -0500

    Betty added

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * reverse_listint - Reverses a listint_t linked list
+ * @head: Head of the list
+ *
+ * Return: Tail of the list as head
+ **/
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev, *current, *next;
@@ -7,7 +13,8 @@ listint_t *reverse_listint(listint_t **head)
 	prev = next = NULL;
 	current = *head;
 
-	while (current != NULL) {
+	while (current != NULL)
+	{
 		next = current->next;
 		current->next = prev;
 		prev = current;
