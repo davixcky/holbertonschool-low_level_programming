427048ae359a8ee6752aac996750e941c589320f
David Orozco <dvdizcky@gmail.com>
2020-03-24 20:42:27 -0500

    Betty style added

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,10 +1,13 @@
 #include "lists.h"
 
+/**
+ * find_listint_loop - Finds the loop in a linked list
+ * @head: Head of the list
+ *
+ * Return: Node when start the loop
+ **/
 listint_t *find_listint_loop(listint_t *head)
 {
-
-	if (head == NULL)
-		return (NULL);
 
 	while (head != NULL)
 	{
