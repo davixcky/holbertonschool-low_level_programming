618e6fcf130161346077501406dad5760d61dcbc
David Orozco <dvdizcky@gmail.com>
2020-03-26 15:34:14 -0500

    Exercise 1 added

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,6 +2,9 @@
 #define HOLBERTON_H
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
+int _putchar(char);
 
 #endif
