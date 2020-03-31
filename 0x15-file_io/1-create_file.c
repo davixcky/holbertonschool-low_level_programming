677b2e5977a0dde20d7812a4bfa5c991b0b48185
David Orozco <dvdizcky@gmail.com>
2020-03-31 14:59:20 -0500

    Return in condition of text changed to 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 
 	for (i = 0; text_content[i] != 0; i++)
 		;
@@ -22,4 +22,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (res == -1 ? -1 : 1);
 }
-
