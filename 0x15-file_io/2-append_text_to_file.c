36078c91989bfe2cade61ff34574dcb6dfca0658
David Orozco <dvdizcky@gmail.com>
2020-03-31 18:34:25 -0500

    betty style applied

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * append_text_to_file - Appends text at the end of a file
+ * @filename: Name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
+ **/
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i;
