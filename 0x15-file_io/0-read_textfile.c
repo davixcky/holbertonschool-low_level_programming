ca83dafbf5742c93133b1ae2f9d888078fe8e811
David Orozco <dvdizcky@gmail.com>
2020-03-31 12:54:15 -0500

    Task 1 added

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  int fd, n_bytes, status;
+  char *buffer;
+
+  if (filename == NULL)
+    return (0);
+
+  fd = open(filename, O_RDONLY);
+  if (fd < 0)
+    return (0);
+
+  buffer = malloc(letters * sizeof(char));
+  if (buffer == NULL)
+    return (0);
+
+  n_bytes = read(fd, buffer, letters);
+  if (n_bytes == 0)
+  {
+    free(buffer);
+    return (0);
+  }
+
+  status = write(1, buffer, n_bytes);
+  free(buffer);
+
+  return (status);
+}
+
+
+
+
+
