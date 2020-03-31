d16c36ab7f866facceb5b312fa23dda34c1318e4
David Orozco <dvdizcky@gmail.com>
2020-03-31 13:58:01 -0500

    Replaced to symbolic constant

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,7 +27,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     return (0);
   }
 
-  status = write(1, buffer, n_bytes);
+  status = write(STDOUT_FILENO, buffer, n_bytes);
   free(buffer);
 
   return (status);
