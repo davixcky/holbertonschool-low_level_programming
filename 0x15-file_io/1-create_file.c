d1cda2d3600d3172cba6dc6b4a533ec13961b77d
David Orozco <dvdizcky@gmail.com>
2020-03-31 15:02:10 -0500

    Betty style added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * create_file - Creates a file
+ * @filename: Name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ **/
 int create_file(const char *filename, char *text_content)
 {
 	int fd, i, res;
