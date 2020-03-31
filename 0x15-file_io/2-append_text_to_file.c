6fbba0d65b4a56d51d4c829fcfdd492c33b267c7
David Orozco <dvdizcky@gmail.com>
2020-03-31 15:58:30 -0500

    NULL condition added

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,6 +11,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
 	for (i = 0; text_content[i] != 0; i++)
 		;
 
