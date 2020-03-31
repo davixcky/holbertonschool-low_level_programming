bbd03e49219db99a483ae4cac90d9d8722ccdac2
David Orozco <dvdizcky@gmail.com>
2020-03-31 14:55:45 -0500

    Task 1 added

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, res;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+
+	for (i = 0; text_content[i] != 0; i++)
+		;
+
+	res = write(fd, text_content, i);
+	close(fd);
+
+	return (res == -1 ? -1 : 1);
+}
+
