b35dbb7e21fbe0083a39133755af8d89af907a92
David Orozco <dvdizcky@gmail.com>
2020-05-02 11:22:51 -0500

    Task 2: Set previous to the head (current) node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -23,6 +23,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->prev = NULL;
 	new_node->next = *head;
 	new_node->n = n;
+
+	if (*head)
+		(*head)->prev = new_node;
+
 	*head = new_node;
 
 	return (new_node);
