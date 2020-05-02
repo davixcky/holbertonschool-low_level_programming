2887a388c04a12809bd01e29c91850abf61241a1
David Orozco <dvdizcky@gmail.com>
2020-05-02 12:01:34 -0500

    Task 7: Support to 0 index and lenght + 1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,28 +18,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	for (index = 0; *h != NULL; h = &(*h)->next, index++)
-	{
 		if (index == idx)
-		{
-			current = (*h)->prev;
+			break;
 
-			new_node = malloc(sizeof(dlistint_t));
-			if (new_node == NULL)
-				return (NULL);
+	if (index == idx + 1 || index != idx)
+		return (NULL);
 
-			new_node->n = n;
-			new_node->prev = current;
-			new_node->next = current ? current->next : *h;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-			if (current)
-				current->next = new_node;
-			else
-				*h = new_node;
-			/*h = &current*/
+	new_node->n = n;
 
-			return (new_node);
-		}
-	}
+	current = *h ? (*h)->prev : NULL;
 
-	return (NULL);
+	new_node->prev = current;
+	new_node->next = current ? current->next : *h;
+
+	if (current)
+		current->next = new_node;
+	else
+		*h = new_node;
+
+	return (new_node);
 }
