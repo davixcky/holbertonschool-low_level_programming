4931abfdc733d6beae309334fc7061dbc3114407
David Orozco <dvdizcky@gmail.com>
2020-05-29 12:48:34 -0500

    Betty style applied

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a hash table
+ *
+ * @size: Numbers of buckets to create
+ *
+ * Return: New hash_table or NULL if failed
+ **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - Gives you the index of a key
+ *
+ * @key: Key to calculate
+ * @size: Number of buckets/slots
+ *
+ * Return: Index of key
+ **/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
