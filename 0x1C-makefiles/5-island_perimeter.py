fef77728f812c871f15b583447a65b9002f2b94f
David Orozco <dvdizcky@gmail.com>
2020-07-03 19:07:59 -0500

    Task 5 added

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for task 5"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    total = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                total += 4
+                if i >= 0 and grid[i - 1][j] == 1:
+                    total -= 2
+
+                if i >= 0 and grid[i][j - 1] == 1:
+                    total -= 2
+
+    return total
