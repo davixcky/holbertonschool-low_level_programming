0b817814657d706533c57307dc65576dc90eb666
David Orozco <dvdizcky@gmail.com>
2020-07-03 19:16:24 -0500

    Task 5: Different size in row support

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,13 +7,13 @@ def island_perimeter(grid):
     total = 0
 
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
+        for j in range(len(grid[i])):
             if grid[i][j]:
                 total += 4
-                if i >= 0 and grid[i - 1][j] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     total -= 2
 
-                if i >= 0 and grid[i][j - 1] == 1:
+                if j > 0 and grid[i][j - 1] == 1:
                     total -= 2
 
     return total
